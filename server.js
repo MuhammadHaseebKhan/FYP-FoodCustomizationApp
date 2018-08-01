08322382dba0c7df4a6df95685d4a300aa90a7a1
haseebsudofy <haseeb@sudofy.com>
2018-08-02 01:18:36 +0500

    make it better

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/foodistan'));
+app.use(express.static(__dirname + '/dist/food-customization'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/foodistan/index.html'));
+res.sendFile(path.join(__dirname+'/dist/food-customization/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
